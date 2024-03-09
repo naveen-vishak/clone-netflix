1ee9a45c8a593225ff5a504361385af44f5224f5
naveen-vishak <naveenvishakvijayakumar@gmail.com>
2024-03-09 13:15:53 +0530

    Login form input validation

--- README.md
+++ README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# clone-netflix-gpt
+- Create React app
+- Configure tailwind
+- Routing
+- Header
+- Login && Sign up
+- Form Validation
+- useRef hook
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,62 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
+import { validateEmail, validatePassword, validateUserName } from "../utils/validateForm";
 
 const LoginForm = () => {
     const [signedIn, setSgnedIn] = useState(false);
+    const [emailMessage, setEmailMessage] = useState(null);
+    const [userNameMessage, setUserNameMessage] = useState(null);
+    const [passwordMessage, setPasswordMessage] = useState(null);
+    const email = useRef(null);
+    const userName = useRef(null);
+    const password = useRef(null);
+
+    const Validate = () => {
+        setEmailMessage(validateEmail(email?.current?.value));
+        setUserNameMessage(validateUserName(userName?.current?.value));
+        setPasswordMessage(validatePassword(password?.current?.value));
+    }
+
     const handleSignIn = () => {
         setSgnedIn(!signedIn)
     }
     return (
     <div className="">
-        <form className='bg-black w-[35%] mx-auto right-0 left-0 text-white px-[100px] py-[60px] rounded-lg bg-opacity-70'>
+        <form onSubmit={e => e.preventDefault()} className='bg-black w-[35%] mx-auto right-0 left-0 text-white px-[100px] py-[60px] rounded-lg bg-opacity-60'>
             <h1 className="text-3xl mb-4 font-bold">
                 { signedIn ? "Sign in" : "Sign up" }
             </h1>
-            { !signedIn && <input className="w-[100%] p-2 my-4 rounded-md bg-gray-300 text-black" type="text" placeholder=" Name"/>}
-            <input className="w-[100%] p-2 my-4 rounded-md bg-gray-300 text-black" type="text" placeholder=" Email"/>
-            <input className="w-[100%] p-2 my-4 rounded-md bg-gray-300 text-black" type="password" placeholder=" Password"/>
-            <button className="w-[100%] p-2 my-4 mt-7 rounded-md bg-red-800 font-bold">
+
+            {!signedIn && 
+            <div className="py-4">
+                {userNameMessage && <p className="pb-1 text-sm text-red-800 cursor-pointer">{userNameMessage}</p>}
+                <input ref={userName} className="w-[100%] p-2 rounded-md bg-gray-200 text-black" type="text" placeholder=" Name"/>
+            </div>}
+            
+            <div className="py-4">
+                {emailMessage && <p className="pb-1 text-sm text-red-800 cursor-pointer">{emailMessage}</p>}
+                <input ref={email} className="w-[100%] p-2 rounded-md bg-gray-200 text-black" type="text" placeholder=" Email"/>
+            </div>
+
+            <div className="py-4">
+                {passwordMessage && <p className="pb-1 text-sm text-red-800 cursor-pointer">{passwordMessage}</p>}
+                <input ref={password} className="w-[100%] p-2 rounded-md bg-gray-200 text-black" type="password" placeholder=" Password"/>
+                {passwordMessage && !signedIn &&
+                <ul className="pt-1 text-sm text-red-800 cursor-pointer">
+                    <li>Must have,</li>
+                    <li> - Minimum 8 characters in length.</li>
+                    <li> - At least one uppercase English letter.</li>
+                    <li> - At least one lowercase English letter.</li>
+                    <li> - At least one digit.</li>
+                    <li> - At least one special character</li>
+                </ul>}
+            </div>
+            
+            <button 
+            className="w-[100%] p-2 my-4 mt-8 rounded-md bg-red-800 font-bold"
+            onClick={Validate}>
                 { signedIn ? "Sign in" : "Sign up" }
             </button>
+            
             <p className="text-xs my-2 text-gray-400 cursor-pointer" onClick={handleSignIn}>
                 { signedIn ? "New to Netflix? Sign up now" : "Already have an account? Sign in" }
             </p>
